054e327131044361f9b389ce49769588c763bf58
MadShak <57186531+MadShak@users.noreply.github.com>
2023-03-28 09:44:02 -0300

    Add files via upload

---
+++ brain/src/app/core/services/da-vinci.js
@@ -0,0 +1,34 @@
+//BY SHAK
+
+const chat = document.getElementById('chat');
+const form = document.getElementById('form');
+const loading = document.getElementById("loading");
+
+if (form !== null) {
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const message = document.getElementById('message').value;
+    chat.innerHTML += `<p><strong style="color: red;">@Pinky:</strong> ${message}</p>`;
+    document.getElementById("message").value = "";
+    loading.innerHTML = '<img src="src/assets/img/load-ico.gif" alt="loading" width="50" height="50">';
+
+    // Request
+    fetch('https://api.openai.com/v1/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 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'
+      },
+      body: JSON.stringify({
+        prompt: message,
+        model: 'text-davinci-003',
+        max_tokens: 4000
+      })
+    }).then(response => response.json())
+      .then(data => {
+        const text = data.choices;
+        loading.innerHTML = "";
+        chat.innerHTML += `<p><strong style="color: blue;">@Brain:</strong> ${text[0].text}</p>`;
+      });
+  });
+}
